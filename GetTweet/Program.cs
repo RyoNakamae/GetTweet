4db18e899cb67e0d26188d43ef743079b345882b
RyoNakamae <ryo.nakamae@gmail.com>
2016-12-30 17:11:08 +0900

    いったん完成。 スクリーン名、キーワード指定で取得したTweetを指定ファイルに出力する

--- GetTweet/Program.cs
+++ GetTweet/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 
 namespace GetTweet
 {
@@ -12,37 +6,31 @@ namespace GetTweet
     {
         static void Main(string[] args)
         {
-
-            #region 認証
-            var api_key = ConfigurationManager.AppSettings["api_key"];
-            var api_secret = ConfigurationManager.AppSettings["api_secret"];
-            var token = ConfigurationManager.AppSettings["token"];
-            var token_secret = ConfigurationManager.AppSettings["token_secret"];
-
-            var tokens = CoreTweet.Tokens.Create(api_key, api_secret, token, token_secret);
-            #endregion
+            var tweet = new Tweet();
 
             #region ユーザ指定で取得
-            var screenName = ConfigurationManager.AppSettings["screen_name"];
+            var screenNames = ConfigurationManager.AppSettings["screen_names"];
 
-            var parm = new Dictionary<string, object>();
-            parm["count"] = 100;
-            parm["screen_name"] = screenName;
-
-            var tweets = tokens.Statuses.UserTimeline(parm);
-
-            //所定のファイルに追記する
-            using (StreamWriter sw = new StreamWriter(ConfigurationManager.AppSettings["OutFilePath"], true, Encoding.UTF8))
+            if (!string.IsNullOrEmpty(screenNames))
             {
-                foreach (var tweet in tweets)
+                foreach (var user in screenNames.Split(','))
                 {
-                    Console.WriteLine("{0}: {1}",tweet.User.ScreenName,tweet.Text);
-                    Console.WriteLine("---------");
-                    sw.WriteLine(tweet.Text);
+                    tweet.GetByUser(user);
                 }
             }
             #endregion
 
+            #region キーワード指定で取得
+            var keywords = ConfigurationManager.AppSettings["keywords"];
+
+            if (!string.IsNullOrEmpty(keywords))
+            {
+                foreach (var keyword in keywords.Split(','))
+                {
+                    tweet.GetByKeyword(keyword);
+                }
+            }
+            #endregion
         }
     }
 }
---
+++ GetTweet/Tweet.cs
@@ -0,0 +1,88 @@
+﻿using CoreTweet;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text;
+
+namespace GetTweet
+{
+    class Tweet
+    {
+        Tokens tokens;
+
+        public Tweet()
+        {
+            #region 認証
+            var api_key = ConfigurationManager.AppSettings["api_key"];
+            var api_secret = ConfigurationManager.AppSettings["api_secret"];
+            var token = ConfigurationManager.AppSettings["token"];
+            var token_secret = ConfigurationManager.AppSettings["token_secret"];
+
+            tokens = CoreTweet.Tokens.Create(api_key, api_secret, token, token_secret);
+            #endregion
+        }
+
+        public void GetByUser(string screen_name)
+        {
+            #region ユーザ指定で取得
+            var screenNames = ConfigurationManager.AppSettings["screen_names"];
+
+            var parm = new Dictionary<string, object>();
+            parm["count"] = 10;
+
+            parm["screen_name"] = screen_name;
+
+            var tweets = tokens.Statuses.UserTimeline(parm);
+            #endregion
+
+            //所定のファイルに追記する
+            using (StreamWriter sw = new StreamWriter(ConfigurationManager.AppSettings["OutFilePath"], true, Encoding.UTF8))
+            {
+                foreach (var tweet in tweets)
+                {
+                    Console.WriteLine("{0}: {1}", tweet.User.ScreenName, tweet.Text);
+                    Console.WriteLine("---------");
+                    sw.WriteLine(replace(tweet.Text));
+                }
+            }
+        }
+
+        public void GetByKeyword(string keyword)
+        {
+            var tweets = tokens.Search.Tweets(count => 10, q => keyword);
+
+            //所定のファイルに追記する
+            using (StreamWriter sw = new StreamWriter(ConfigurationManager.AppSettings["OutFilePath"], true, Encoding.UTF8))
+            {
+                foreach (var tweet in tweets)
+                {
+                    Console.WriteLine("{0}: {1}", tweet.User.ScreenName, tweet.Text);
+                    Console.WriteLine("---------");
+                    sw.WriteLine(replace(tweet.Text));
+                }
+            }
+
+
+        }
+
+        string replace(string text)
+        {
+            var res = text;
+
+            //ハッシュタグ以降をのぞく
+            if (res.Contains("#"))
+            {
+                res = res.Remove(res.IndexOf('#'));
+            }
+
+            //リンク以降を除く
+            if (res.Contains("https"))
+            {
+                res = res.Remove(res.IndexOf("https"));
+            }
+
+            return res;
+        }
+    }
+}
